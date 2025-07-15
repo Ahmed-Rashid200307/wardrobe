d4d8db9639798110a9a2ae4fc0637188daf2a600
Ahmed-Rashid200307 <ahmedrashid2003.07@gmail.com>
2025-07-15 23:57:56 +0500

    added variant input option in the same place as dress input

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Dress,Category,Variant
 
+class VarInline(admin.StackedInline):
+    model = Variant
+    
+    extra = 3
+
+class DressAdmin(admin.ModelAdmin):
+    inlines = [VarInline]
 # Register your models here.
-admin.site.register(Dress)
 admin.site.register(Category)
-admin.site.register(Variant)
+admin.site.register(Dress, DressAdmin)
+# admin.site.register(Variant)
